d1df5a32608690131b30ee9f5adcf3a1cf4c1849
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-03 14:42:58 -0700

    verify stock qty vs user qty working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,10 +15,14 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+//declares variable to hold number of items in database
+var numOfItems = 0;
+
 //makes product query from database
 connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
     if (err) throw err;
     //uses console.table to format the returned query in a readable table
+    numOfItems = res.length;
     var table = cTable.getTable(res);
     console.log("\nProducts avaliable to purchase\n------------------------------\n" + table);
     buy();
@@ -30,19 +34,44 @@ function buy() {
         .prompt([{
             name: "item",
             type: "input",
-            message: "What is the id of the product you would like to buy?"
+            message: "What is the id of the product you would like to buy?",
+            //makes sure the the value entered it a positive integer that is avaliable on the items to buy table.
+            validate: function(value) {
+                var integer = Number.isInteger(parseFloat(value));
+                var positive = Math.sign(value);
+                if(integer && (positive === 1) && (value <= numOfItems)) {
+                    return true;
+                } else {
+                    return "Please select a valid item id."
+                }
+            }
         },
         {
             name: "qty",
             type: "input",
             message: "How many would you like to buy?",
+            //makes sure the value entered is a positive integer
+            validate: function(value) {
+                var integer = Number.isInteger(parseFloat(value));
+                var positive = Math.sign(value);
+                if(integer && (positive === 1)) {
+                    return true;
+                } else {
+                    return "Please enter a number."
+                }
+            }
         }]).then(function (input) {
             //takes user input and makes sure there is enough of a product to fulfill the users order.
             var item = input.item;
             var qty = input.qty;
             connection.query("SELECT * FROM products WHERE ?", {item_id: item}, function(err,res) {
                 if (err) throw err;
-                console.log(res);
+                var stock = res[0].stock_quantity;
+                if (qty <= stock) {
+                    console.log("there is enough in stock");
+                } else {
+                    console.log("there isn't enough in stock");
+                }
             })
         })
 }
