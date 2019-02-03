670f32bf8cac1edb2281540d5fa77b67e001f2cd
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-03 15:19:20 -0700

    stock qty updating

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,8 +21,8 @@ var numOfItems = 0;
 //makes product query from database
 connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
     if (err) throw err;
-    //uses console.table to format the returned query in a readable table
-    numOfItems = res.length;
+    numOfItems = res.length;//assigns number of items in database
+    //uses console.table to format the returned query in a readable table   
     var table = cTable.getTable(res);
     console.log("\nProducts avaliable to purchase\n------------------------------\n" + table);
     buy();
@@ -35,11 +35,12 @@ function buy() {
             name: "item",
             type: "input",
             message: "What is the id of the product you would like to buy?",
-            //makes sure the the value entered it a positive integer that is avaliable on the items to buy table.
-            validate: function(value) {
+            validate: function (value) {
+                //makes sure the the value entered it a positive integer that is avaliable on the items to buy table.
                 var integer = Number.isInteger(parseFloat(value));
                 var positive = Math.sign(value);
-                if(integer && (positive === 1) && (value <= numOfItems)) {
+
+                if (integer && (positive === 1) && (value <= numOfItems)) {
                     return true;
                 } else {
                     return "Please select a valid item id."
@@ -49,12 +50,13 @@ function buy() {
         {
             name: "qty",
             type: "input",
-            message: "How many would you like to buy?",
-            //makes sure the value entered is a positive integer
-            validate: function(value) {
+            message: "How many would you like to buy?",           
+            validate: function (value) {
+                 //makes sure the value entered is a positive integer
                 var integer = Number.isInteger(parseFloat(value));
                 var positive = Math.sign(value);
-                if(integer && (positive === 1)) {
+
+                if (integer && (positive === 1)) {
                     return true;
                 } else {
                     return "Please enter a number."
@@ -64,13 +66,22 @@ function buy() {
             //takes user input and makes sure there is enough of a product to fulfill the users order.
             var item = input.item;
             var qty = input.qty;
-            connection.query("SELECT * FROM products WHERE ?", {item_id: item}, function(err,res) {
-                if (err) throw err;
-                var stock = res[0].stock_quantity;
-                if (qty <= stock) {
-                    console.log("there is enough in stock");
-                } else {
-                    console.log("there isn't enough in stock");
+            connection.query("SELECT * FROM products WHERE item_id = ?", item, function (err, res) {
+                if (err) throw err;                
+                var result = res[0];//query result                
+                var stock = result.stock_quantity;//number in stock from query                
+                var changeQty = stock - qty;//difference between the qty in stock and the user request qty               
+                var price = result.price * qty;//total price of requested qty
+                
+                if (qty <= stock) {//if stock is greater then the user request...
+                    //update the qty in the database, subtracting the number the user bought.
+                    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [changeQty, item]);
+                    console.log("\nPurchase successful! You're total was $" + price + ".");
+                    //end connection to the server.
+                    connection.end();               
+                } else {//if stock is less than user request, let user know there wasn't enough, then have them retry their purchase.
+                    console.log("\nThere isn't enough of that product in stock. Please restart your purchase.\n");
+                    buy();
                 }
             })
         })
