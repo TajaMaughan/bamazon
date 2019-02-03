8c50174d11a17d62bcea204b9f90fa6f43cb5184
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-03 11:53:57 -0700

    item prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cTable = require("console.table");
 var password = process.env.PASSWORD;
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,13 +11,29 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected")
+    list();
 })
 
-connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-})
+function list() {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+        if (err) throw err;
+        result = res;
+        var table = cTable.getTable(res);
+        console.log(table);
+    })
+    buy();
+}
+
+function buy() {
+    inquirer
+        .prompt({
+            name: "buy",
+            type: "input",
+            message: "What is the id of the product you would like to buy?",
+            validate: function() {
+                return (buy !== NaN);
+            }
+        })
+}
